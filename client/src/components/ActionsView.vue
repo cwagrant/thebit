57379a501aa5cc2f66c2b77d1413c71a6326cc81
Grant J. Butler <grantjbutler@users.noreply.github.com>
2025-12-24 16:46:55 -0500

    Don't add overal controller title to the path.

--- client/src/components/ActionsView.vue
+++ client/src/components/ActionsView.vue
@@ -3,26 +3,30 @@
   import ActionsView from './ActionsView.vue';
   import ActionView from './ActionView.vue';
   
-  const { title, path } = defineProps<{
+  const { pathComponent, path } = defineProps<{
     path: string[],
+    pathComponent?: string,
     title: string,
     actions: Actions,
     controller: "obs" | "atem";
   }>();
 
-  const fullPath = path.concat([title]);
+  let fullPath = path;
+  if (pathComponent) {
+    path.push(pathComponent);
+  }
 </script>
 <template>
   <div>
     <p>{{ title }}</p>
     <div v-if="Array.isArray(actions)">
       <div v-for="action in actions">
-        <ActionView :action="action" :path="fullPath" :controller="controller" />
+        <ActionView :action="action" :path-component="title" :path="fullPath" :controller="controller" />
       </div>
     </div>
     <div v-else-if="typeof actions === 'object'">
       <div v-for="(mapping, title) in actions">
-        <ActionsView :title="title" :actions="mapping" :path="fullPath" :controller="controller" />
+        <ActionsView :title="title" :path-component="title" :actions="mapping" :path="fullPath" :controller="controller" />
       </div>
     </div>        
   </div>
